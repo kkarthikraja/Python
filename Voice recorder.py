dbd27f513c754d4f4692a14056ee496cee4086af
Karthikraja Karunakaran <kkarthikraja1997@gmail.com>
2023-01-25 13:24:19 +0100

    Add files via upload

---
+++ Voice recorder.py
@@ -0,0 +1,16 @@
+"""
+Author: Karthikraja
+
+Voice recorder Program 
+"""
+import sounddevice
+from scipy.io.wavfile import write
+
+def voice_recorder(seconds, file):
+    print("Recording Started…")
+    recording = sounddevice.rec((seconds * 44100), samplerate= 44100, channels=2)
+    sounddevice.wait()
+    write(file, 44100, recording)
+    print("Recording Finished")
+
+voice_recorder(10, "record.wav")
