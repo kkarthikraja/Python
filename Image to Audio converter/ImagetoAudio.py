832b52dd17aaac37e6257e4e04b3862a972a60ca
Karthikraja Karunakaran <kkarthikraja1997@gmail.com>
2023-03-28 13:58:30 +0200

    Add files via upload

---
+++ Image to Audio converter/ImagetoAudio.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Mar 28 13:51:50 2023
+Image to Audio converter using Python
+@author: kkarthikraja
+"""
+
+from PIL import Image
+from gtts import gTTS
+from pytesseract import image_to_string
+
+
+def image_to_sound(path_to_image):
+    """
+    Function for converting an  image to sound
+    """
+    try:
+        loaded_image = Image.open(path_to_image)
+        decoded_text = image_to_string(loaded_image)
+        cleaned_text = " ".join(decoded_text.split("\n"))
+        print(cleaned_text)
+        sound = gTTS(cleaned_text, lang="en")
+        sound.save("sound.mp3")
+        return True
+    except Exception as bug:
+        print("The bug thrown while excuting the code\n", bug)
+        return
+
+
+if __name__ == "__main__":
+    image_to_sound("image.jpg")
+    input()
